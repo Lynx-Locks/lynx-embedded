e2e07b351c7cd3718b82e3b08b5301fa5f1e31a5
Cameron Kinsella <ckinsella@uwaterloo.ca>
2024-03-08 21:23:26 -0500

    Compile and link ykhmac components in esp-idf-sys

---
+++ components/ykhmac/include/ykhmac.h
@@ -0,0 +1,222 @@
+/**
+ * @file ykhmac.h
+ * @author Christoph Honal
+ * @brief Defines the Yubikey HMAC-SHA1 logic
+ * @version 0.1
+ * @date 2021-12-17
+ */
+
+#ifndef YKHMAC_H
+#define YKHMAC_H
+
+#include <stdbool.h>
+#include "aes.h"
+
+#define YKHMAC_DEBUG
+
+
+// Debugging
+#ifdef YKHMAC_DEBUG
+    /**
+     * @brief Prints a zero-terminated string to a debug output
+     *
+     * @param message The message to print
+     */
+    extern void ykhmac_debug_print(const char* message);
+
+    // On AVR Arduinos, use PROGMEM to store strings
+    #ifdef ARDUINO_ARCH_AVR
+        class __FlashStringHelper;
+
+        /**
+         * @brief Prints a Arduino PROGMEM string to a debug output
+         *
+         * @param message The message to print
+         */
+        extern void ykhmac_debug_print(const __FlashStringHelper* message);
+    #else
+        #define F(A) A
+    #endif
+#endif
+
+// Helpers
+#define MAX(x, y)               (((x) > (y)) ? (x) : (y))       //!< Maximum of two numbers
+#define MIN(x, y)               (((x) < (y)) ? (x) : (y))       //!< Minimum of two numbers
+
+// Hardware limits
+#ifndef HW_BUF_SIZE
+    #define HW_BUF_SIZE         64                              //!< Size of the transfer buffer of the NFC controller used
+#endif
+#ifndef SEND_BUF_OVERH
+    #define SEND_BUF_OVERH      2                               //!< Overhead of the send buffer in bytes
+#endif
+#define SEND_BUF_SIZE           (HW_BUF_SIZE - SEND_BUF_OVERH)  //!< Usable space of the transfer buffer for sending
+#ifndef RECV_BUF_OVERH
+    #define RECV_BUF_OVERH      8                               //!< Overhead of the send buffer in bytes
+#endif
+#define RECV_BUF_SIZE           (HW_BUF_SIZE - RECV_BUF_OVERH)  //!< Usable space of the transfer buffer for receiving
+#define ARG_BUF_SIZE_MAX        (SEND_BUF_SIZE - 5)             //!< Maximum size of an ADPU without header
+#ifndef RESP_BUF_SIZE
+    #define RESP_BUF_SIZE       20                              //!< Size of the response buffer (inherent to SHA1)
+#endif
+#ifndef SECRET_KEY_SIZE
+    #define SECRET_KEY_SIZE     20                              //!< Size of the secret key
+#endif
+#define SECRET_KEY_SIZE_PAD     (((SECRET_KEY_SIZE / AES_BLOCKLEN) + 1) * AES_BLOCKLEN) //!< Size of the secret key, padded for AES
+#ifndef CHALLENGE_SIZE
+    #define CHALLENGE_SIZE      ARG_BUF_SIZE_MAX                //!< Size of the generated challenges, max. ARG_BUF_SIZE_MAX
+#endif
+#ifndef EEPROM_SIZE
+    #define EEPROM_SIZE CHALLENGE_SIZE + AES_BLOCKLEN + SECRET_KEY_SIZE_PAD + 1
+#endif
+
+// Response codess
+#define E_SUCCESS                   0 //!< Operation was successfull
+#define E_UNEXPECTED                1 //!< Unexpected error occurred (protocol violation)
+#define E_CARD_NOT_AUTHENTICATED    2 //!< Token requires user interaction / unlocking
+#define E_FILE_NOT_FOUND            3 //!< The applet with the specified AID was not found
+
+// Slot IDs
+#define SLOT_1 1 //!< Configuration slot 1
+#define SLOT_2 2 //!< Configuration slot 2
+
+// APDU definitions
+#define CLA_ISO             0x00 //!< Default ISO command class
+#define INS_SELECT          0xA4 //!< Select applet instruction
+#define SEL_APP_AID         0x04 //!< Select by AID parameter
+#define INS_STATUS          0x03 //!< Status request instruction
+#define INS_API_REQ         0x01 //!< API request instruction
+#define CMD_GET_SERIAL      0x10 //!< Read serial number API command
+#define CMD_HMAC_1          0x30 //!< Compute HMAC on slot 1 API command
+#define CMD_HMAC_2          0x38 //!< Compute HMAC on slot 2 API command
+
+#define SW_OK_HIGH          0x90 //!< Successfull response code, high byte
+#define SW_OK_LOW           0x00 //!< Successfull response code, low byte
+#define SW_PRECOND_HIGH     0x69 //!< Precondition failed error response code, high byte
+#define SW_PRECOND_LOW      0x85 //!< Precondition failed error response code, low byte
+#define SW_NOTFOUND_HIGH    0x6A //!< Not found error response code, high byte
+#define SW_NOTFOUND_LOW     0x82 //!< Not found error response code, low byte
+#define SW_UNSUP_HIGH       0x6D //!< Unsupported operation error response code, high byte
+
+// AIDs
+#define YUBIKEY_AID_LENGTH  7 //!< Size of the official Yubico applet AID
+#define YUBIKEY_AID         { 0xA0, 0x00, 0x00, 0x05, 0x27, 0x20, 0x01 } //!< Official Yubico applet AID
+#define FIDESMO_AID_LENGTH  11 //!< Size of a Fidesmo development applet AID
+#define FIDESMO_AID         { 0xA0, 0x00, 0x00, 0x06, 0x17, 0x00, 0x07, 0x53, 0x4E, 0xAF, 0x01 } //!< Fidesmo development applet AID
+
+/**
+ * @brief Prototype declaration of NFC hardware interfacing function
+ *
+ * @param send_buffer Buffer to be sent to the target
+ * @param send_length Amount of bytes to be sent
+ * @param response_buffer Buffer to be read from the target
+ * @param response_length Amount of bytes to be read
+ * @return true on success
+ */
+extern bool ykhmac_data_exchange(uint8_t *send_buffer, uint8_t send_length,
+    uint8_t* response_buffer, uint8_t* response_length);
+
+/**
+ * @brief Prototype declaration of random number generator
+ *
+ * @return Random byte
+ */
+extern uint8_t ykhmac_random();
+
+/**
+ * @brief Prototype declaration of a persistent write function
+ *
+ * @param data  Buffer to be written from
+ * @param size Amount of bytes to be written
+ * @param offset Where to write the bytes to
+ * @return true on success
+ */
+extern bool ykhmac_presistent_write(const uint8_t *data, const size_t size, const size_t offset);
+
+/**
+ * @brief Prototype declaration of a persistent read function
+ *
+ * @param data  Buffer to be read into
+ * @param size Amount of bytes to be read
+ * @param offset Where to read the bytes from
+ * @return true on success
+ */
+extern bool ykhmac_presistent_read(uint8_t *data, const size_t size, const size_t offset);
+
+/**
+ * @brief Selects an applet by its AID
+ *
+ * @param aid The AID of the applet
+ * @param aid_size The length of the AID in bytes
+ * @return true on success
+ */
+bool ykhmac_select(const uint8_t* aid, const uint8_t aid_size);
+
+
+/**
+ * @brief Reads the serial number of the target
+ *
+ * @param serial The serial number
+ * @return true on success
+ */
+bool ykhmac_read_serial(uint32_t* serial);
+
+/**
+ * @brief Reads the firmware version of the target
+ *
+ * @param version The firmware version
+ * @return true on success
+ */
+bool ykhmac_read_version(uint8_t version[3]);
+
+/**
+ * @brief Performs a HMAC-SHA1 challenge-response exchange with the target
+ *
+ * @param slot Which slot to use, either SLOT_1 or SLOT_2
+ * @param challenge Input buffer, contains challenge
+ * @param challenge_length Size of the input buffer in bytes, max. ARG_BUF_SIZE_MAX
+ * @param response Output buffer, contains response. May be nullptr to discard response
+ * @return true on success
+ */
+bool ykhmac_exchange_hmac(const uint8_t slot, const uint8_t* challenge,
+    const uint8_t challenge_length, uint8_t response[RESP_BUF_SIZE]);
+
+/**
+ * @brief Tests both slots of the target for valid configurations
+ *
+ * @return SLOT_1 | SLOT_2
+ */
+uint8_t ykhmac_find_slots();
+
+/**
+ * @brief Enrolls a secret key into encrypted persistent memory
+ *
+ * @param secret_key The secret key to be enrolled
+ * @return true on success
+ */
+bool ykhmac_enroll_key(uint8_t secret_key[SECRET_KEY_SIZE]);
+
+/**
+ * @brief tries to authenticate a target against the stored secret key
+ *
+ * In addition, this function will advance the stored secret key.
+ *
+ * @param slot Which slot to use, either SLOT_1 or SLOT_2
+ *
+ * @return true on successful authentication
+ */
+bool ykhmac_authenticate(const uint8_t slot);
+
+/**
+ * @brief Computes a HMAC-SHA1 response using a secret key and challenge
+ *
+ * @param key Secret key buffer, size must be at least SECRET_KEY_SIZE
+ * @param challenge Input buffer, contains challenge
+ * @param challenge_length Size of the input buffer in bytes, max. ARG_BUF_SIZE_MAX
+ * @param response Output buffer, contains response. Must be at least RESP_BUF_SIZE
+ * @return true on success
+ */
+bool ykhmac_compute_hmac(const uint8_t* key, const uint8_t* challenge,
+    const uint8_t challenge_length, uint8_t response[RESP_BUF_SIZE]);
+
+#endif
---
+++ components/ykhmac/ykhmac.c
@@ -0,0 +1,422 @@
+/**
+ * @file ykhmac.cpp
+ * @author Christoph Honal
+ * @brief Implements the definitions from ykhmac.h
+ * @version 0.1
+ * @date 2021-12-17
+ */
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "ykhmac.h"
+
+#include "sha1.h"
+#include "aes.h"
+
+#ifdef YKHMAC_DEBUG
+        // Print array as string to some debug output
+    #ifdef ARDUINO_ARCH_AVR
+        void ykhmac_debug_print_array(const __FlashStringHelper* prefix,
+            const uint8_t* data, const size_t size)
+    #else
+        void ykhmac_debug_print_array(const char* prefix, const uint8_t* data,
+            const size_t size)
+    #endif
+        {
+            ykhmac_debug_print(prefix);
+            char hxdig[4] = {0};
+            for (size_t i = 0; i < size; i++)
+            {
+                sprintf(hxdig, "%02x ", data[i]);
+                ykhmac_debug_print(hxdig);
+            }
+            ykhmac_debug_print("\n");
+        }
+#endif
+
+
+// Decode APDU response code
+uint8_t ykhmac_response_code(const uint8_t *recv_buffer, const uint8_t recv_length)
+{
+    if (recv_length < 2)
+    {
+        return E_UNEXPECTED;
+    }
+
+    if (recv_buffer[recv_length - 2] == SW_OK_HIGH &&
+        recv_buffer[recv_length - 1] == SW_OK_LOW)
+    {
+        return E_SUCCESS;
+    }
+
+    if (recv_buffer[recv_length - 2] == SW_PRECOND_HIGH &&
+        recv_buffer[recv_length - 1] == SW_PRECOND_LOW)
+    {
+        return E_CARD_NOT_AUTHENTICATED;
+    }
+
+    if (recv_buffer[recv_length - 2] == SW_NOTFOUND_HIGH &&
+        recv_buffer[recv_length - 1] == SW_NOTFOUND_LOW)
+    {
+        return E_FILE_NOT_FOUND;
+    }
+
+    return E_UNEXPECTED;
+}
+
+
+bool ykhmac_select(const uint8_t *aid, const uint8_t aid_size)
+{
+    if (aid_size > ARG_BUF_SIZE_MAX) return false;
+
+    // Communication buffers
+    uint8_t send_buffer[5 + aid_size];
+    uint8_t recv_length = 12;
+    uint8_t recv_buffer[recv_length];
+
+    // Setup command buffers
+    send_buffer[0] = CLA_ISO;
+    send_buffer[1] = INS_SELECT;
+    send_buffer[2] = SEL_APP_AID;
+    send_buffer[3] = 0;
+    send_buffer[4] = aid_size;
+    memcpy(send_buffer + 5, aid, aid_size);
+
+    // Perform transfer
+    if (ykhmac_data_exchange(send_buffer, 5 + aid_size, recv_buffer, &recv_length))
+    {
+        return ykhmac_response_code(recv_buffer, recv_length) == E_SUCCESS;
+    }
+
+    return false;
+}
+
+bool ykhmac_read_serial(uint32_t *serial)
+{
+    // Communication buffers
+    uint8_t send_buffer[5];
+    uint8_t recv_length = 6;
+    uint8_t recv_buffer[recv_length];
+
+    // Setup command buffers
+    send_buffer[0] = CLA_ISO;
+    send_buffer[1] = INS_API_REQ;
+    send_buffer[2] = CMD_GET_SERIAL;
+    send_buffer[3] = 0;
+    send_buffer[4] = 6;
+
+    // Perform transfer
+    if (ykhmac_data_exchange(send_buffer, 5, recv_buffer, &recv_length))
+    {
+        if (ykhmac_response_code(recv_buffer, recv_length) == E_SUCCESS && recv_length >= 4)
+        {
+            *serial = ((uint32_t)recv_buffer[0] << 24) + ((uint32_t)recv_buffer[1] << 16) +
+                      ((uint32_t)recv_buffer[2] << 8) + recv_buffer[3];
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ykhmac_read_version(uint8_t version[3])
+{
+    // Communication buffers
+    uint8_t send_buffer[5];
+    uint8_t recv_length = 8;
+    uint8_t recv_buffer[recv_length];
+
+    // Setup command buffers
+    send_buffer[0] = CLA_ISO;
+    send_buffer[1] = INS_STATUS;
+    send_buffer[2] = 0;
+    send_buffer[3] = 0;
+    send_buffer[4] = 6;
+
+    // Perform transfer
+    if (ykhmac_data_exchange(send_buffer, 5, recv_buffer, &recv_length))
+    {
+        if (ykhmac_response_code(recv_buffer, recv_length) == E_SUCCESS && recv_length >= 3)
+        {
+            memcpy(version, recv_buffer, 3);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ykhmac_exchange_hmac(const uint8_t slot, const uint8_t *challenge,
+                          const uint8_t challenge_length, uint8_t response[RESP_BUF_SIZE])
+{
+
+
+    if (challenge_length > ARG_BUF_SIZE_MAX)
+        return false;
+
+    uint8_t slot_cmd = 0;
+    if (slot == SLOT_1)
+        slot_cmd = CMD_HMAC_1;
+    else if (slot == SLOT_2)
+        slot_cmd = CMD_HMAC_2;
+    else
+        return false;
+
+    // Communication buffers
+    uint8_t send_buffer[challenge_length + 5];
+    uint8_t recv_length = RESP_BUF_SIZE + 2;
+    uint8_t recv_buffer[recv_length];
+
+    // Setup command buffers
+    send_buffer[0] = CLA_ISO;
+    send_buffer[1] = INS_API_REQ;
+    send_buffer[2] = slot_cmd;
+    send_buffer[3] = 0;
+    send_buffer[4] = challenge_length;
+    memcpy(send_buffer + 5, challenge, challenge_length);
+
+    // Perform transfer
+    if (ykhmac_data_exchange(send_buffer, 5 + challenge_length, recv_buffer, &recv_length))
+    {
+        if (ykhmac_response_code(recv_buffer, recv_length) == E_SUCCESS
+            && recv_length >= RESP_BUF_SIZE)
+        {
+            if (response != NULL) memcpy(response, recv_buffer, RESP_BUF_SIZE);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+uint8_t ykhmac_find_slots()
+{
+    uint8_t slots = 0;
+
+    // Perform dummy challenge agains both slots
+    uint8_t challenge[8];
+    memset(challenge, 0x42, 8);
+
+    if (ykhmac_exchange_hmac(SLOT_1, challenge, 8, NULL))
+        slots |= SLOT_1;
+    if (ykhmac_exchange_hmac(SLOT_2, challenge, 8, NULL))
+        slots |= SLOT_2;
+
+    return slots;
+}
+
+// Common buffers to save RAM
+struct sha1_hasher_s sha_context;
+uint8_t challenge[CHALLENGE_SIZE];
+uint8_t response[RESP_BUF_SIZE];
+uint8_t iv[AES_BLOCKLEN];
+uint8_t padded_secret_key[SECRET_KEY_SIZE_PAD];
+struct AES_ctx aes_context;
+uint8_t computed_response[RESP_BUF_SIZE];
+
+void ykhmac_purge_buffers()
+{
+    // Purge data from RAM
+    memset(challenge, 0, CHALLENGE_SIZE);
+    memset(response, 0, RESP_BUF_SIZE);
+    memset(iv, 0, AES_BLOCKLEN);
+    memset(padded_secret_key, 0, SECRET_KEY_SIZE_PAD);
+    memset(&aes_context, 0, sizeof(struct AES_ctx));
+    memset(computed_response, 0, RESP_BUF_SIZE);
+}
+
+bool ykhmac_compute_hmac(const uint8_t *key, const uint8_t *challenge,
+                         const uint8_t challenge_length, uint8_t response[RESP_BUF_SIZE])
+{
+    // Init hasher using key and data
+    sha1_hasher_init_hmac(&sha_context, key, SECRET_KEY_SIZE);
+    for (uint8_t i = 0; i < challenge_length; i++)
+    {
+        if (sha1_hasher_putc(&sha_context, challenge[i]) == EOF)
+            return false;
+    }
+
+    // Compute and return hash
+    uint8_t *result = sha1_hasher_gethmac(&sha_context);
+    memcpy(response, result, RESP_BUF_SIZE);
+
+    // Purge hasher RAM
+    memset(&sha_context, 0, sizeof(struct sha1_hasher_s));
+
+    return true;
+}
+
+bool ykhmac_enroll_key(uint8_t secret_key[SECRET_KEY_SIZE])
+{
+    #ifdef YKHMAC_DEBUG
+        ykhmac_debug_print(F("Enrolling key\n"));
+        ykhmac_debug_print_array(F("Using secret key:     "), secret_key, SECRET_KEY_SIZE);
+    #endif
+
+    bool result = false;
+
+    // Generate random challenge
+    for (uint8_t i = 0; i < CHALLENGE_SIZE; i++) challenge[i] = ykhmac_random();
+    #ifdef YKHMAC_DEBUG
+        ykhmac_debug_print_array(F("Random challenge:     "), challenge, CHALLENGE_SIZE);
+    #endif
+
+    // Compute response
+    if (ykhmac_compute_hmac(secret_key, challenge, CHALLENGE_SIZE, response))
+    {
+        #ifdef YKHMAC_DEBUG
+            ykhmac_debug_print_array(F("Computed response:    "), response, RESP_BUF_SIZE);
+        #endif
+
+        // Pad secret key using zeros (fixed size)
+        memset(padded_secret_key + SECRET_KEY_SIZE, 0, SECRET_KEY_SIZE_PAD - SECRET_KEY_SIZE_PAD);
+        memcpy(padded_secret_key, secret_key, SECRET_KEY_SIZE);
+        #ifdef YKHMAC_DEBUG
+            ykhmac_debug_print_array(F("Padded secret key:    "), padded_secret_key, SECRET_KEY_SIZE_PAD);
+        #endif
+
+        // Encrypt secret key using response as encryption key
+        for (uint8_t i = 0; i < AES_BLOCKLEN; i++) iv[i] = ykhmac_random();
+        #ifdef YKHMAC_DEBUG
+            ykhmac_debug_print_array(F("Using IV:             "), iv, AES_BLOCKLEN);
+        #endif
+        AES_init_ctx_iv(&aes_context, response, iv);
+        AES_CBC_encrypt_buffer(&aes_context, padded_secret_key, SECRET_KEY_SIZE_PAD);
+        #ifdef YKHMAC_DEBUG
+            ykhmac_debug_print_array(F("Encrypted secret key: "), padded_secret_key, SECRET_KEY_SIZE_PAD);
+        #endif
+
+        // Store challenge, IV and encrypted secret key
+        if (ykhmac_presistent_write(challenge, CHALLENGE_SIZE, 0)
+            && ykhmac_presistent_write(iv, AES_BLOCKLEN, CHALLENGE_SIZE)
+            && ykhmac_presistent_write(padded_secret_key, SECRET_KEY_SIZE_PAD,
+                CHALLENGE_SIZE + AES_BLOCKLEN))
+        {
+            #ifdef YKHMAC_DEBUG
+                ykhmac_debug_print(F("Wrote data to persistent storage\n"));
+            #endif
+            result = true;
+        }
+        else
+        {
+            #ifdef YKHMAC_DEBUG
+                ykhmac_debug_print(F("Failed to write data to persistent storage\n"));
+            #endif
+        }
+    }
+    else
+    {
+        #ifdef YKHMAC_DEBUG
+            ykhmac_debug_print(F("Failed to compute HMAC\n"));
+        #endif
+    }
+
+    ykhmac_purge_buffers();
+
+    #ifdef YKHMAC_DEBUG
+        if (result)
+            ykhmac_debug_print(F("Successfully enrolled key\n"));
+        else
+            ykhmac_debug_print(F("Failed to enroll key\n"));
+    #endif
+
+    return result;
+}
+
+bool ykhmac_authenticate(const uint8_t slot)
+{
+    #ifdef YKHMAC_DEBUG
+        ykhmac_debug_print(F("Authenticating key\n"));
+    #endif
+
+    bool result = false;
+
+    // Load stored challenge
+    if (ykhmac_presistent_read(challenge, CHALLENGE_SIZE, 0))
+    {
+        #ifdef YKHMAC_DEBUG
+            ykhmac_debug_print_array(F("Loaded challenge:     "), challenge, CHALLENGE_SIZE);
+        #endif
+
+        // Perform challenge-response exchange
+        if (ykhmac_exchange_hmac(slot, challenge, CHALLENGE_SIZE, response))
+        {
+            #ifdef YKHMAC_DEBUG
+                ykhmac_debug_print_array(F("Exchanged response:   "), response, RESP_BUF_SIZE);
+            #endif
+
+            // Load IV and secret key
+            if (ykhmac_presistent_read(iv, AES_BLOCKLEN, CHALLENGE_SIZE)
+                && ykhmac_presistent_read(padded_secret_key, SECRET_KEY_SIZE_PAD,
+                    CHALLENGE_SIZE + AES_BLOCKLEN))
+            {
+                #ifdef YKHMAC_DEBUG
+                    ykhmac_debug_print_array(F("Loaded IV:            "), iv, AES_BLOCKLEN);
+                    ykhmac_debug_print_array(F("Loaded secret key:    "), padded_secret_key, SECRET_KEY_SIZE_PAD);
+                #endif
+
+                // Decrypt secret key
+                AES_init_ctx_iv(&aes_context, response, iv);
+                AES_CBC_decrypt_buffer(&aes_context, padded_secret_key, SECRET_KEY_SIZE_PAD);
+                #ifdef YKHMAC_DEBUG
+                    ykhmac_debug_print_array(F("Decrypted secret key: "), padded_secret_key, SECRET_KEY_SIZE_PAD);
+                #endif
+
+                // Compute response using secret key
+                if (ykhmac_compute_hmac(padded_secret_key, challenge, CHALLENGE_SIZE, computed_response))
+                {
+                    #ifdef YKHMAC_DEBUG
+                        ykhmac_debug_print_array(F("Computed response:    "), computed_response, RESP_BUF_SIZE);
+                    #endif
+
+                    // Check response
+                    if (memcmp(response, computed_response, RESP_BUF_SIZE) == 0)
+                    {
+                        #ifdef YKHMAC_DEBUG
+                            ykhmac_debug_print(F("Responses match\n"));
+                        #endif
+
+                        // Perform re-enrollment and re-encryption of the secret using a new challenge
+                        result = ykhmac_enroll_key(padded_secret_key);
+                    }
+                    else
+                    {
+                        #ifdef YKHMAC_DEBUG
+                            ykhmac_debug_print(F("Responses do not match\n"));
+                        #endif
+                    }
+                }
+                else
+                {
+                    #ifdef YKHMAC_DEBUG
+                        ykhmac_debug_print(F("Failed to compute HMAC\n"));
+                    #endif
+                }
+            }
+            else
+            {
+                #ifdef YKHMAC_DEBUG
+                    ykhmac_debug_print(F("Failed to read data from persistent storage\n"));
+                #endif
+            }
+        }
+        else
+        {
+            #ifdef YKHMAC_DEBUG
+                ykhmac_debug_print(F("Failed to exchange HMAC\n"));
+            #endif
+        }
+    }
+
+    ykhmac_purge_buffers();
+
+    #ifdef YKHMAC_DEBUG
+        if (result)
+            ykhmac_debug_print(F("Successfully authenticated token\n"));
+        else
+            ykhmac_debug_print(F("Failed to authenticate token\n"));
+    #endif
+
+    return result;
+}
